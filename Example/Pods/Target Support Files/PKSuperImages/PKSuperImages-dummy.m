2ecf0d270f90e069881fb6a8ee9f733c0948f770
pawan421 <pawan@appster.in>
2018-12-02 01:37:19 +0530

    Initial commit with pod lib template

---
+++ Example/Pods/Target Support Files/PKSuperImages/PKSuperImages-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_PKSuperImages : NSObject
+@end
+@implementation PodsDummy_PKSuperImages
+@end
---
+++ Example/Pods/Target Support Files/Pods-PKSuperImages_Example/Pods-PKSuperImages_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_PKSuperImages_Example : NSObject
+@end
+@implementation PodsDummy_Pods_PKSuperImages_Example
+@end
